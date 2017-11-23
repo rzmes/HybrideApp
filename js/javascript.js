84b25baedec97b7334a3f37282fa45921cb5f174
Ricardo Ruiz-Zorrilla Morales <RI_RU@ec4u.de>
2017-11-23 11:01:15 +0100

    Seitenwechsel eingefügt

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+function functionPage1() {
+    var page1 = document.getElementById("page1");
+    page1.className = "active";
+
+    var page2 = document.getElementById("page2");
+    page2.className = "";
+
+    var page3 = document.getElementById("page3");
+    page3.className = "";
+}
+
+function functionPage2() {
+    var page1 = document.getElementById("page1");
+    page1.className = "";
+
+    var page2 = document.getElementById("page2");
+    page2.className = "active";
+
+    var page3 = document.getElementById("page3");
+    page3.className = "";
+}
+
+function functionPage3() {
+    var page1 = document.getElementById("page1");
+    page1.className = "";
+
+    var page2 = document.getElementById("page2");
+    page2.className = "";
+
+    var page3 = document.getElementById("page3");
+    page3.className = "active";
+}
